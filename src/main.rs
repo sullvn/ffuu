1b2010e193b0b97450dd51630da4a52b509dbf4e
Kevin Sullivan <kevin@sull.vn>
2020-07-19 12:27:53 -0700

    Minimum markdown parsing test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use pulldown_cmark::{Parser, html};
+
+const MARKDOWN: &str = "This is **bold**.";
+
 fn main() {
-    println!("Hello, world!");
+    let md_parser = Parser::new(MARKDOWN);
+    let mut html_output = String::new();
+
+    html::push_html(&mut html_output, md_parser);
+    println!("{}", html_output);
 }

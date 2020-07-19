a5321718ae924ec837992d6439d47519f28ecda1
Kevin Sullivan <kevin@sull.vn>
2020-07-19 16:13:36 -0700

    Read input file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use std::env;
+use std::str;
+use async_std::fs::{read, write};
 use pulldown_cmark::{Parser, html};
 
-const MARKDOWN: &str = "This is **bold**.";
+#[async_std::main]
+async fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let input_path = args.get(1).unwrap();
 
-fn main() {
-    let md_parser = Parser::new(MARKDOWN);
+    let input_bytes = read(input_path).await?;
+    let input_text: &str = str::from_utf8(&input_bytes).unwrap();
+
+    let md_parser = Parser::new(input_text);
     let mut html_output = String::new();
 
     html::push_html(&mut html_output, md_parser);
     println!("{}", html_output);
+
+    Ok(())
 }

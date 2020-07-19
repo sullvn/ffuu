474e5a0a74ea049bd52f94831540024b0aa310e2
Kevin Sullivan <kevin@sull.vn>
2020-07-19 10:58:22 -0700

    Initialize Cargo

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  nixpkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
+in
+  with nixpkgs;
+  mkShell {
+    buildInputs = [
+      nixpkgs.latest.rustChannels.stable.rust
+    ];
+  }

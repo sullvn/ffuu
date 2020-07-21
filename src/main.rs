7c8904dbec6fd35516851e40f21f30b24c3a324a
Kevin Sullivan <kevin@sull.vn>
2020-07-20 22:45:42 -0700

    Print top-level HTML tags

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use async_std::path::Path;
 use async_std::task::spawn;
 use async_std::io;
 use futures::future;
-use pulldown_cmark::{Parser, html};
+use pulldown_cmark::{Parser, html, Event, CowStr};
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
@@ -44,8 +44,9 @@ async fn write_html<P: AsRef<Path>, Q: AsRef<Path>>(input_path: P, output_dir: Q
     let input_bytes = read(input_path).await?;
     let input_text: &str = str::from_utf8(&input_bytes)?;
 
-    let mut html_output = String::new();
+    do_parse(input_text);
 
+    let mut html_output = String::new();
     {
         let md_parser = Parser::new(input_text);
         html::push_html(&mut html_output, md_parser);
@@ -54,4 +55,70 @@ async fn write_html<P: AsRef<Path>, Q: AsRef<Path>>(input_path: P, output_dir: Q
     write(output_path, html_output).await?;
 
     Ok(())
+}
+
+fn do_parse(text: &str) {
+    let mut md_parser = Parser::new(text);
+    let mut depth: isize = 0;
+    let mut curr_tag: Option<&str> = None;
+
+    while let Some(event) = md_parser.next() {
+        if let Event::Html(CowStr::Borrowed(tag)) = event {
+            if let Some(HtmlTag { name, direction }) = parse_tag(&tag) {
+                match (depth, &direction, curr_tag) {
+                    (0,  TagDirection::Open, _) => {
+                        curr_tag = Some(name);
+                    },
+                    (1, TagDirection::Close, Some(curr_tag_name)) => {
+                        println!("{}", curr_tag_name);
+                        curr_tag = None;
+                    },
+                    _ => {},
+                };
+
+                depth += direction.depth_change();
+            }
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Debug)]
+enum TagDirection {
+    Open,
+    Close,
+}
+
+impl TagDirection {
+    fn depth_change(&self) -> isize {
+        match self {
+            TagDirection::Open => 1,
+            TagDirection::Close => -1,
+        }
+    }
+}
+
+struct HtmlTag<'a> {
+    name: &'a str,
+    direction: TagDirection,
+}
+
+
+fn parse_tag<'a>(s: &'a str) -> Option<HtmlTag<'a>> {
+    let without_prefix = s.trim().strip_prefix('<')?;
+    let name_end = without_prefix.find(|c: char|
+        c.is_whitespace() || c == '>')?;
+    let name = without_prefix.get(0..name_end)?;
+
+    let html_tag = match name.strip_prefix('/') {
+        None => HtmlTag {
+            name,
+            direction: TagDirection::Open,
+        },
+        Some(name) => HtmlTag {
+            name,
+            direction: TagDirection::Close,
+        },
+    };
+
+    Some(html_tag)
 }

8d0a9415a45cac5bfb6adae80e2f213822c871f3
Kevin Sullivan <kevin@sull.vn>
2020-10-03 16:46:36 -0700

    Prepare output directory

--- ffuu-cli/src/main.rs
+++ ffuu-cli/src/main.rs
@@ -1,9 +1,26 @@
+use async_std::fs::{create_dir_all, remove_dir_all};
+use async_std::io;
+
 mod args;
 
 use args::Arguments;
 
-fn main() {
+#[async_std::main]
+async fn main() -> anyhow::Result<()> {
     let args = Arguments::parse();
-    println!("{:?}", args.input_file_path());
-    println!("{:?}", args.output_dir_path());
+    prepare_output_dir(args.output_dir_path()).await?;
+
+    Ok(())
+}
+
+/// Prepare output directory
+///
+/// Clear and create it.
+///
+async fn prepare_output_dir(path: &str) -> io::Result<()> {
+    match remove_dir_all(path).await {
+        Err(err) if err.kind() == io::ErrorKind::NotFound => Ok(()),
+        r => r,
+    }?;
+    create_dir_all(path).await
 }

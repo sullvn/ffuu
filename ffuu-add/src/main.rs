c48dcda7632a73e54db2a512689abcc6a4ac05b1
Kevin Sullivan <kevin@sull.vn>
2021-05-08 18:20:20 -0700

    Use `FFUU_OUTPUT_DIR` env var

--- ffuu-add/src/main.rs
+++ ffuu-add/src/main.rs
@@ -1,7 +1,13 @@
 use async_std::io;
+use std::env;
+
+const OUTPUT_DIR_ENV_KEY: &str = "FFUU_OUTPUT_DIR";
 
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
+    let output_dir = env::var(OUTPUT_DIR_ENV_KEY)?;
+    println!("{}", output_dir);
+
     let mut stdin = io::stdin();
     let mut stdout = io::stdout();
 

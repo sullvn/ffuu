7fdb1b38bdf7cdd8f3ff92cd095e0d68d9769283
Kevin Sullivan <kevin@sull.vn>
2021-05-08 22:19:39 -0700

    Output resulting filename in `ffuu-add`

--- ffuu-add/src/main.rs
+++ ffuu-add/src/main.rs
@@ -4,6 +4,7 @@ use args::Arguments;
 use async_std::fs::{create_dir_all, File};
 use async_std::io;
 use async_std::path::{Path, PathBuf};
+use io::prelude::WriteExt;
 use std::env;
 
 const OUTPUT_DIR_ENV_KEY: &str = "FFUU_OUTPUT_DIR";
@@ -16,6 +17,10 @@ async fn main() -> anyhow::Result<()> {
     let output_dir: &Path = output_dir_env.as_ref();
     let output_file_path_relative = args.output_file_path();
     let output_file_path: PathBuf = [output_dir, output_file_path_relative].iter().collect();
+    let website_file_path: PathBuf = ["/".as_ref(), output_file_path_relative].iter().collect();
+    let website_file_path_str: &str = website_file_path
+        .to_str()
+        .ok_or(anyhow::anyhow!("Bad filename"))?;
     let output_file_dir: PathBuf = [
         output_dir,
         output_file_path_relative
@@ -25,12 +30,13 @@ async fn main() -> anyhow::Result<()> {
     .iter()
     .collect();
 
-    println!("{:?}", output_file_path);
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
 
     create_dir_all(output_file_dir).await?;
-    let mut stdin = io::stdin();
     let mut output_file = File::create(output_file_path).await?;
-
     io::copy(&mut stdin, &mut output_file).await?;
+    stdout.write_all(website_file_path_str.as_bytes()).await?;
+
     Ok(())
 }
